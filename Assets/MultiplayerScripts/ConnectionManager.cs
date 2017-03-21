7d18359a0ae6392b4d2f4fff3cd16b75e0e0e88e
Nihal369 <nihalismailk@gmail.com>
2017-03-21 21:18:40 +0530

    Single Scene architecture

--- Assets/MultiplayerScripts/ConnectionManager.cs
+++ Assets/MultiplayerScripts/ConnectionManager.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ConnectionManager : MonoBehaviour {
 
 	//Function Purpose
 	//Check whether both client and host joins a particular game
+	public Text testDisplay;
 
 	Player[] players;
 
 	void Update () {
 		players = FindObjectsOfType<Player> ();
 		if (players.Length == 2) {
-			SceneManager.LoadScene ("M1_TEAMS");
+			testDisplay.text = "Adichu mone :D";
 		}
 	}
 }
--- Assets/MultiplayerScripts/Player.cs
+++ Assets/MultiplayerScripts/Player.cs
@@ -2,14 +2,4 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
