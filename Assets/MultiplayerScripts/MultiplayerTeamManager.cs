004dfd754eaebd6460c25e1fd835272857ee974a
Nihal369 <nihalismailk@gmail.com>
2017-03-24 11:55:36 +0530

    Player destorys team scene objects in setting scene

--- Assets/MultiplayerScripts/MultiplayerTeamManager.cs
+++ Assets/MultiplayerScripts/MultiplayerTeamManager.cs
@@ -7,15 +7,26 @@ using UnityEngine.SceneManagement;
 public class MultiplayerTeamManager : MonoBehaviour {
 
 	Player[] players;
+	int numberOfChildrenDestroyed;
 
 	void Start()
 	{
 		players = FindObjectsOfType<Player> ();
+		numberOfChildrenDestroyed = 0;
 	}
 
 	void Update()
 	{
 		if (!string.IsNullOrEmpty (players [0].teamName) && !string.IsNullOrEmpty (players [1].teamName)) {
+			for (int i = 0; i < players.Length; i++) {
+				numberOfChildrenDestroyed = 0;
+				foreach (Transform child in players[i].transform) {
+					if (numberOfChildrenDestroyed < 3) {
+						Destroy (child.gameObject);
+						numberOfChildrenDestroyed++;
+					}
+				}
+			}
 			SceneManager.LoadScene ("M2_SETTINGS");
 		}
 	}
