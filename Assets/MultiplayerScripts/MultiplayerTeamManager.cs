e27fadc0cab26af7fa309d3d2c4dcc0159efc9e7
Nihal369 <nihalismailk@gmail.com>
2017-03-24 11:32:37 +0530

    Setting screen loads

--- Assets/MultiplayerScripts/MultiplayerTeamManager.cs
+++ Assets/MultiplayerScripts/MultiplayerTeamManager.cs
@@ -2,9 +2,21 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class MultiplayerTeamManager : MonoBehaviour {
 
+	Player[] players;
 
+	void Start()
+	{
+		players = FindObjectsOfType<Player> ();
+	}
+
+	void Update()
+	{
+		if (!string.IsNullOrEmpty (players [0].teamName) && !string.IsNullOrEmpty (players [1].teamName)) {
+			SceneManager.LoadScene ("M2_SETTINGS");
+		}
+	}
 }
