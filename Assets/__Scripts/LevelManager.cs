ce531f8613e5a0394bf0f7c25ee18ae550970a9b
Nihal369 <nihalismailk@gmail.com>
2017-04-17 13:02:04 +0530

    Back button added

--- Assets/__Scripts/LevelManager.cs
+++ Assets/__Scripts/LevelManager.cs
@@ -2,12 +2,27 @@
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : NetworkBehaviour {
     
     public void LoadLevel(string levelName)
     {
 		SceneManager.LoadScene (levelName);
     }
-		
+
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (SceneManager.GetActiveScene ().buildIndex != 1 && SceneManager.GetActiveScene ().buildIndex != 9
+			    && SceneManager.GetActiveScene ().buildIndex != 15 && SceneManager.GetActiveScene ().buildIndex != 10) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
+			} else if (SceneManager.GetActiveScene ().buildIndex == 10) {
+				NetworkManager manager = FindObjectOfType<NetworkManager> ();
+				Destroy (manager.gameObject);
+				SceneManager.LoadScene (1);
+			}
+		}
+	}
 }
